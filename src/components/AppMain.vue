b42ef0088a13178718a0bf6620e5bb6d30663f99
Leo9723 <pala9761@gmail.com>
2023-01-16 01:00:46 +0100

    aggiunta bonus di effetto caricamento

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,15 +19,26 @@ export default {
         axios.get(store.url).then((response) => {
           console.log(response.data.data)
           store.CardList = response.data.data
+          store.loading = true
         })
       }
     }
 }
 </script>
 <template lang="">
-    <div>
-    <div class="maincont">
+    <div class="all">
+    <div class="maincont" v-if="store.loading">
         <Card v-for="(card, index) in store.CardList" :card="card" :key="index" :img="card.card_images" />
+    </div>
+    <div v-else>
+        <div class="row">
+            <div>
+                <i class="loading-icon fas fa-circle-notch"></i>
+                <div class="loading-text">
+                    CARICAMENTO IN CORSO...
+                </div>
+            </div>
+        </div>
     </div>
     </div>
 </template>
@@ -43,6 +54,28 @@ export default {
         .cardContainer{
             background-color: white;
             width: 20%;
+        }
+     }
+     .row {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        div{
+            text-align: center;
+            .loading-text{
+                font-weight: bold;
+                font-size: x-large;
+            }
+            .loading-icon{
+           animation: rotella 150s infinite;
+           font-size: 100px;
+           margin-bottom: 50px;
+             }
+              @keyframes rotella {
+                0% {transform: rotate(100turn);}
+                }
         }
      }
 </style>
